fa2adc8661154ff6978791a954c9f64a45c102b8
Nada Elmokdem <167814957+nadaaelmokdem@users.noreply.github.com>
2026-07-01 15:39:19 +0300

    JJ

--- Backend/obj/Debug/net10.0/Tabibi.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Tabibi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tabibi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90e20a26e5e82d9f56682e98b6d5f16beb095f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef8ec167c100486e056255ad7b04419cbfde4fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tabibi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tabibi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
